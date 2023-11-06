47e4a38245b50301bccf9c9a76d3eb843cbbf9e2
rebleo <rml444@nyu.edu>
2023-11-06 11:23:02 -0500

    javascript

--- week09/code/theMedium.js
+++ week09/code/theMedium.js
@@ -1,1 +1,30 @@
 console.log("theMessage");
+
+const theButton = document.querySelector("button");
+const theInput = document.querySelector("input")
+let theDivider = document.querySelector("div")
+
+let theUserWords;
+let theItem;
+let theTxt;
+
+theButton.addEventListener('click', makeList)
+
+function makeList() {
+    console.log("click!");
+    theUserWords = theInput.value;
+    // console.log(theUserWords)
+    theItem = document.createElement('li')
+    const theSpan = document.createElement('span');
+    theTxt = document.createTextNode(theUserWords);
+    // document.body.appendChild(theItem);
+    theDivider.appendChild(theItem);
+    theItem.appendChild(theTxt);
+}
+
+document.querySelector("p").addEventListener('click', eraseList)
+
+function eraseList(){
+    console.log("erase!");
+    document.querySelector("div").remove(theItem)
+}
