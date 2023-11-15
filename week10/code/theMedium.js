a8ee826cd03eae4b11da7cec94fe8a0a2a477a52
rebleo <rml444@nyu.edu>
2023-11-15 11:52:40 -0500

    data day

--- week10/code/theMedium.js
+++ week10/code/theMedium.js
@@ -1,1 +1,28 @@
-console.log("theMessage")
+console.log("theMessage");
+
+let theData;
+let theColor;
+let theString;
+let theTxt = document.querySelector("p")
+
+document.querySelector("body").addEventListener('click',whatIsThis);
+
+function preload(){
+    theData = loadJSON('crayola.json')
+}
+
+function setup(){
+    noLoop();
+    noCanvas();
+    console.log(theData.colors)
+}
+
+function whatIsThis(){
+    let theRandom = random(theData.colors)
+    console.log(theRandom.hex)
+    console.log("you clicked!")
+    theColor = theRandom.hex;
+    document.body.style.backgroundColor = theColor;
+    theTxt.textContent = theString;
+    theString = theRandom.color;
+}
--- week10/code2/theServer.js
+++ week10/code2/theServer.js
@@ -10,3 +10,21 @@ let theTxt;
 const theButton = document.querySelector("button").addEventListener("click", findMeaning)
 // fetch api
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+async function findMeaning(){
+    console.log("click");
+    theWord = theContainer.value;
+    const theResponse = await fetch("https://api.wordnik.com/v4/word.json/" + theWord + "/definitions?limit=200&includeRelated=false&sourceDictionaries=all&useCanonical=false&includeTags=false&api_key=31692cf32bc7c1b39f40d0feef50d73c7e07334a11433664c")
+    console.log(theResponse)
+    const theMeanings = await theResponse.json();
+    console.log(theMeanings);
+    for (let i=0; i< theMeanings.length; i++){
+        console.log(theMeanings[0].text)
+        theTxt = document.createTextNode(theMeanings[0].text)
+        // need to debug the following line
+        // theContainer.appendChild(theText)
+    }
+    console.log(theTxt);
+    console.log(typeof theTxt);
+    
+}
